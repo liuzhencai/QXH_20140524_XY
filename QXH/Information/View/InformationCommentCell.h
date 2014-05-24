69e26f8e4dc2084ccc21a9a6623fdb022fe3b5e8
qxh <admin@221.179.130.169>
2014-05-24 14:30:07 +0800

    合并版本

---
+++ QXH/Information/View/InformationCommentCell.h
@@ -0,0 +1,13 @@
+//
+//  InformationCommentCell.h
+//  QXH
+//
+//  Created by ZhaoLilong on 5/18/14.
+//  Copyright (c) 2014 ZhaoLilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationCommentCell : UITableViewCell
+
+@end
