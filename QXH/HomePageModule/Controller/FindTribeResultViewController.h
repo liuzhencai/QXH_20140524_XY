cf2d6d280e6a99aa7bab172f95c756e3ddebd08a
Desktop <playzxw@xueyong.local>
2014-07-08 22:34:11 +0800

    修改 8、部落搜索，放在所有部落里，所有部落，改名为推荐部落

---
+++ QXH/HomePageModule/Controller/FindTribeResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  FindTribeResultViewController.h
+//  QXH
+//
+//  Created by XueYong on 7/8/14.
+//  Copyright (c) 2014 ZhaoLilong. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface FindTribeResultViewController : MyViewController
+@property (nonatomic, strong) NSMutableArray *allTribeList;
+@end
--- QXH/HomePageModule/Controller/TribeController.h
+++ QXH/HomePageModule/Controller/TribeController.h
@@ -10,5 +10,4 @@
 
 @interface TribeController : MyViewController
 @property (strong, nonatomic)  UISearchBar *searchBar;
-- (void)btnClick:(id)sender;
 @end
