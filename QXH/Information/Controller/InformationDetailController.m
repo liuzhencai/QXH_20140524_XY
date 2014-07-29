c6ba6518131b50d893bc2a88e3d5dc35b896037d
ZhaoLilong <zhaolilong2012@gmail.com>
2014-07-29 09:03:52 +0800

    修复智谷详情转发次数不显示的问题

--- QXH/Information/Controller/InformationDetailController.m
+++ QXH/Information/Controller/InformationDetailController.m
@@ -28,7 +28,7 @@
     _articleTitleLabel.text = detailmodel.title;
     _readNumLabel.text = detailmodel.browsetime;
     _commentNumLabel.text = detailmodel.commenttime;
-    _postLabel.text = detailmodel.sharetime;
+    _postLabel.text = detailmodel.relaytime;
     _dateLabel.text = detailmodel.date;
     _praiseLabel.text = [NSString stringWithFormat:@"%d",detailmodel.laud];
     
