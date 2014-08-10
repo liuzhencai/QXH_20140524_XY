2193da0ea9fe03a32bbfd0258ca6bfd305666de0
ZhaoLilong <zhaolilong2012@gmail.com>
2014-08-10 18:29:20 +0800

    修复开启组播失败问题

--- QXH/ThirdParty/CocoaAyncSocket/CGD/GCDAsyncUdpSocket.m
+++ QXH/ThirdParty/CocoaAyncSocket/CGD/GCDAsyncUdpSocket.m
@@ -1962,7 +1962,7 @@ enum GCDAsyncUdpSocketConfig
 		}
 		
 		int reuseaddr = 1;
-		status = setsockopt(socketFD, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
+		status = setsockopt(socket6FD, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
 		if (status == -1)
 		{
 			if (errPtr)
