f16652f626bb856be68b7c0288acf4f666194f79
ZhaoLilong <zhaolilong2012@gmail.com>
2014-08-27 23:54:03 +0800

    修复广场最热点击后跳转显示错误问题

--- QXH/HomePageModule/Controller/SquareViewController.m
+++ QXH/HomePageModule/Controller/SquareViewController.m
@@ -389,7 +389,13 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    SquareInfo *model = [squareInfoList objectAtIndex:indexPath.row];
+    SquareInfo *model = nil;
+    if (_curIndex == 1) {
+        model = [squareInfoList objectAtIndex:indexPath.row];
+    }else{
+        model = [hotestInfoList objectAtIndex:indexPath.row];
+    }
+
     switch (model.type) {
         case 1:
         {
